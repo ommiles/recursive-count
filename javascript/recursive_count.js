dc480b90d79ff2c37bedbc2c43056f970ba95f62
OM Miles <miles@ommiles.com>
2021-05-24 13:34:58 -0700

    algos complete

--- javascript/recursive_count.js
+++ javascript/recursive_count.js
@@ -1,5 +1,7 @@
 function recursiveCount(num = 0) {
-  // type your code here
+  if(num >= 10) {return}
+  console.log(num)
+  recursiveCount(++num)
 }
 
 if (require.main === module) {
--- ruby/recursive_count.rb
+++ ruby/recursive_count.rb
@@ -1,5 +1,7 @@
 def recursive_count(num = 0)
-  # type your code in here
+  return if num >= 10
+  puts num
+  recursive_count(num + 1)
 end
 
 if __FILE__ == $PROGRAM_NAME
